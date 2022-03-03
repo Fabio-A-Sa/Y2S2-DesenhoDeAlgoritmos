b104add70de48a9ee84d05cf9d5672d0f01ce2ad
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-03 21:53:14 +0000

    some notes and tests

--- Notes/1 - Greedy Algorithms.cpp
+++ Notes/1 - Greedy Algorithms.cpp
@@ -1,16 +1,38 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 #include <string>
+#include <algorithm>
 using namespace std;
+
+template <class T>
+void showContent(const vector<T> &content) {
+    for (const T &item : content) {
+        cout << item << " ";
+    } cout << endl;
+}
+
+vector<int> extract(int total, const vector<int> &coins) {
+
+    vector<int> solution = {};
+    priority_queue<int> sortedCoins = {};
+    for (int coin : coins) sortedCoins.push(coin);
+
+    return solution;
+}
 
 void coins() {
 
-
+    vector<int> coins = {1, 1, 1, 2, 2, 2, 2, 5, 5, 10};
+    int total = 8;
+    vector<int> solution = extract(total, coins);
+    showContent(solution);
 
 }
 
 int main () {
 
-    cout << "Hello World!" << endl;
+    coins();
+
     return 0;
 }
