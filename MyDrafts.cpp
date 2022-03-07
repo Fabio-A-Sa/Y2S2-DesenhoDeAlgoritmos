cf96c42e5992c5c27df4527c8f999d5d6f4a55cc
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-07 23:27:52 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -141,3 +141,37 @@ TEST(TP1_Ex1, 3sumNotExists) {
     unsigned int T = 1;
     EXPECT_EQ(sum3(T,selected), false);
 }
+
+nt sumSequence(int A[], int i, int j) {
+    int total = 0;
+    for (; i <= j ; i++) {
+        total += A[i];
+    }
+    return total;
+}
+
+int maxSubsequence(int A[], unsigned int n, unsigned int &i, unsigned int &j) {
+
+    int lower, upper, currentSum = INT_MIN;
+    for (lower = 0 ; lower < n - 1 ; lower++) {
+        for (upper = lower + 1; upper < n ; upper++) {
+            int attemp = sumSequence(A, lower, upper);
+            if (attemp > currentSum) {
+                currentSum = attemp;
+                i = lower;
+                j = upper;
+            }
+        }
+    }
+    return currentSum;
+}
+
+TEST(TP1_Ex2, maxSubsequence) {
+    int A[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4};
+    unsigned int n = 9;
+    unsigned int i, j;
+    EXPECT_EQ(maxSubsequence(A,n,i,j), 6);
+    EXPECT_EQ(i, 3);
+    EXPECT_EQ(j, 6);
+}
+
