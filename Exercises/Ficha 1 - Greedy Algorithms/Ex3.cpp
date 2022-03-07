d72f19e8858e2685f2418f854294eac67066d286
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-07 23:13:18 +0000

    exercise 3

---
+++ Exercises/Ficha 1 - Greedy Algorithms/Ex3.cpp
@@ -0,0 +1,73 @@
+// Created on March, 2022
+// @author: Fábio Araújo de Sá
+
+#include "exercises.h"
+#include <gtest/gtest.h>
+
+int sumValues(vector<int> results, int i , int j) {
+    int total = 0;
+    for (; i <= j ; i++) total += results[i];
+    return total;
+}
+
+bool changeMakingBF(unsigned int C[], unsigned int Stock[], unsigned int n, unsigned int T, unsigned int usedCoins[]) {
+
+    for (int i = 0 ; i < n ; i++) usedCoins[i] = 0;
+
+    for (int i = n - 1 ; i >= 0 && T > 0; i--) {
+        if (Stock[i] > 0 && C[i] <= T) {
+            int nMoedas = std::min(Stock[i], T / C[i]);
+            usedCoins[i] += nMoedas;
+            T -= nMoedas * C[i];
+        }
+    }
+
+    return T == 0;
+}
+
+TEST(TP1_Ex3, hasBFChangeCanonical) {
+    unsigned int C[] = {1,2,5,10};
+    unsigned int Stock[] = {1,1,1,1};
+    unsigned int n = 4;
+    unsigned int usedCoins[4];
+
+    EXPECT_EQ(changeMakingBF(C,Stock,n,13,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 0);
+    EXPECT_EQ(usedCoins[3], 1);
+
+    unsigned int Stock2[] = {1,2,4,2};
+    EXPECT_EQ(changeMakingBF(C,Stock2,n,38,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 1);
+    EXPECT_EQ(usedCoins[2], 3);
+    EXPECT_EQ(usedCoins[3], 2);
+}
+
+TEST(TP1_Ex3, hasBFChangeNonCanonical) {
+    unsigned int C[] = {1,4,5};
+    unsigned int Stock[] = {2,2,1};
+    unsigned int n = 3;
+    unsigned int usedCoins[3];
+
+    EXPECT_EQ(changeMakingBF(C,Stock,n,6,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 1);
+    EXPECT_EQ(usedCoins[1], 0);
+    EXPECT_EQ(usedCoins[2], 1);
+
+    EXPECT_EQ(changeMakingBF(C,Stock,n,8,usedCoins), true);
+    EXPECT_EQ(usedCoins[0], 0);
+    EXPECT_EQ(usedCoins[1], 2);
+    EXPECT_EQ(usedCoins[2], 0);
+}
+
+TEST(TP1_Ex3, hasNoBFChange) {
+    unsigned int C[] = {1,2,5,10};
+    unsigned int Stock[] = {0,1,1,1};
+    unsigned int n = 4;
+    unsigned int usedCoins[4];
+
+    EXPECT_EQ(changeMakingBF(C,Stock,n,18,usedCoins), false);
+    EXPECT_EQ(changeMakingBF(C,Stock,n,1,usedCoins), false);
+}
