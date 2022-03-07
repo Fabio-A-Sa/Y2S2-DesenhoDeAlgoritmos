2e30618b65b8935e9f5feeebe83c5bb215f83074
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-07 23:27:32 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -84,3 +84,60 @@ int main () {
     activities();
     return 0;
 }
+
+bool sum3(unsigned int T, unsigned int nums[3]) {
+
+    for (int i = 0 ; i < T ; i++) {
+        for (int j = 0 ; j < T ; j++) {
+            for (int k = 0 ; k < T ; k++) {
+                if (T == (i+j+k)) {
+                    nums[0] = i;
+                    nums[1] = j;
+                    nums[2] = k;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+bool sum3_a(unsigned int T, unsigned int nums[3]) {
+
+    for (int i = 0 ; i < T ; i++) {
+        for (int j = i ; j < T ; j++) {
+            for (int k = j ; k < T ; k++) {
+                if (T == (i+j+k)) {
+                    nums[0] = i;
+                    nums[1] = j;
+                    nums[2] = k;
+                    return true;
+                }
+            }
+        }
+    }
+    return false;
+}
+
+void compareSums(unsigned int selected[3], unsigned int expectedSum) {
+    EXPECT_EQ(selected[0] + selected[1] + selected[2], expectedSum);
+}
+
+TEST(TP1_Ex1, 3sumExists) {
+    unsigned int selected[3];
+
+    unsigned int T = 10;
+    EXPECT_EQ(sum3(T,selected), true);
+    compareSums(selected,T);
+
+    T = 18;
+    EXPECT_EQ(sum3(T,selected), true);
+    compareSums(selected,T);
+}
+
+TEST(TP1_Ex1, 3sumNotExists) {
+    unsigned int selected[3];
+
+    unsigned int T = 1;
+    EXPECT_EQ(sum3(T,selected), false);
+}
