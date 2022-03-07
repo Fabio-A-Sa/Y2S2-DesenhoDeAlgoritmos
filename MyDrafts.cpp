f113ab5de1fe22e509618f04a87c8f1fafc163b6
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-07 23:29:13 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -326,3 +326,33 @@ TEST(TP1_Ex5, canonicalFalse) {
     unsigned int C[] = {1,4,5};
     EXPECT_EQ(isCanonical(C,3), true);
 }
+
+bool Activity::operator==(const Activity &a2) const {
+    return start == a2.start && finish == a2.finish;
+}
+
+bool Activity::operator<(const Activity &a2) const {
+    return finish < a2.finish;
+}
+
+std::vector<Activity> earliestFinishScheduling(std::vector<Activity> A) {
+
+    sort(A.begin(), A.end());
+    std::vector<Activity> res = {};
+    res.push_back(A[0]);
+    int currentTime = A[0].finish;
+    for (auto act : A) {
+        if (currentTime < act.start) {
+            currentTime = act.finish;
+            res.push_back(act);
+        }
+    }
+    return res;
+}
+
+TEST(TP1_Ex6, activityScheduling) {
+    std::vector<Activity> A = {{10,20}, {30, 35}, {5, 15}, {10, 40}, {40, 50}};
+    std::vector<Activity> V = earliestFinishScheduling(A);
+    EXPECT_EQ(V.size(), 3 );
+    ASSERT_THAT(earliestFinishScheduling(A),  ::testing::ElementsAre(Activity(5, 15), Activity(30, 35), Activity(40, 50)));
+}
