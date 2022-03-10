75cf7e9b89369bc5f07bfa183065771bca6f845c
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-10 01:31:18 +0000

    notes

--- Notes/3 - Divide and Conquer.cpp
+++ Notes/3 - Divide and Conquer.cpp
@@ -6,8 +6,40 @@ int getCoreNumber() {
     return std::thread::hardware_concurrency();
 }
 
+// A <- Array
+// p <- left index
+// r <- right index
+
+void MergeSort(int A[], int p, int r) {
+    if (p < r) {
+        int q = math.floor((p+q) / 2);
+        MergeSort(A, p, q);
+        MergeSort(A, q+1, r);
+        Merge(A, p, q, r);
+    }
+}
+
+void Merge(int A[], int p, int q, int r) {
+
+    // copy -> S(n) = O(n)
+    int L[](p, q);      // left
+    int R[](q+1, r);    // right
+
+    int i = 1, j = 1;
+    for (int k = p ; k < r ; k++) {
+        if (L[i] <= R[i]>) {
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+
+
+
 int main () {
-    cout << "Hello World" << endsl;
-    cout << getCoreNumber() << endl;
+    cout << getCoreNumber() << endl; // 8
     return 0;
 }
