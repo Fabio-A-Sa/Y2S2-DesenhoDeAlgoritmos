f3e7074498008405f7291bcbe08db09e22e8fdea
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-10 01:40:02 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -415,3 +415,62 @@ int main() {
     sumOfSubsets(0, 0);
     return 0;
 }
+
+int getCoreNumber() {
+    return std::thread::hardware_concurrency();
+}
+
+// A <- Array
+// p <- left index
+// r <- right index
+
+void MergeSort(int A[], int p, int r) {
+    if (p < r) {
+        int q = math.floor((p+q) / 2);
+        MergeSort(A, p, q);
+        MergeSort(A, q+1, r);
+        Merge(A, p, q, r);
+    }
+}
+
+// @Overload
+void Merge(int A[], int p, int q, int r) {
+
+    // copy -> S(n) = O(n)
+    int L[](p, q);      // left
+    int R[](q+1, r);    // right
+
+    int i = 1, j = 1;
+    for (int k = p ; k < r ; k++) {
+        if (L[i] <= R[i]>) {
+            A[k] = L[i];
+            i++;
+        } else {
+            A[k] = R[j];
+            j++;
+        }
+    }
+}
+
+// @Overload
+void Mergesort(int A[], int p, int q, int r, int threads) {
+    if (p < r) {
+        int q = math.floor((p+q) / 2);
+        if (threads > 1) {
+            std::thread t ([=]() {          // Lança a função para outro core
+                MergeSort(A, p, q, thread / 2);
+            });
+            MergeSort(A, q+1, r, threads / 2);
+            t.join();                       // Espera que a outra thread termine
+        } else {
+            MergeSort(A, p, q, 1);
+            MergeSort(A, q+1, r, 1);
+        }
+        Merge(A, p, q, r);
+    }
+}
+
+int main () {
+    cout << getCoreNumber() << endl; // 8
+    return 0;
+}
