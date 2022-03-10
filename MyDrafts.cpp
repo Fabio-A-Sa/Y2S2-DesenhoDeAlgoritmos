5c64bf90f2f9ae28e0b44c0f21e9adba92240ba7
Fabio-A-Sa <up202007658@edu.fe.up.pt>
2022-03-10 01:48:09 +0000

    drafts update

--- MyDrafts.cpp
+++ MyDrafts.cpp
@@ -474,3 +474,13 @@ int main () {
     cout << getCoreNumber() << endl; // 8
     return 0;
 }
+
+double power (double x, int n) {
+    if (n == 0) return 1;
+    if (n == 1) return x;
+    else {
+        double d = power(x, n/2);
+        if (n % 2 == 0) return d * d;
+        else return x * d * d;
+    }
+}
